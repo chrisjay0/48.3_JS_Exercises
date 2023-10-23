c2225cd840891c02cdeffe007af480adfac94e7f
Alttaab <alttaab@gmail.com>
2023-10-23 09:12:28 -0700

    Guessing game.

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,25 @@
 function guessingGame() {
+    let randomNumber = Math.floor(Math.random() * 100);
+    let guesses = 0;
+    let hasWon = false;
 
+    return function(guess) {
+        if (hasWon) {
+            return "The game is over, you already won!";
+        }
+        
+        guesses++;
+        
+        if (guess === randomNumber) {
+            hasWon = true;
+            return `You win! You found ${randomNumber} in ${guesses} guesses.`;
+        } else if (guess > randomNumber) {
+            return `${guess} is too high!`;
+        } else {
+            return `${guess} is too low!`;
+        }
+    };
 }
+
 
 module.exports = { guessingGame };
