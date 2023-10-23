80299ff758908000b9a6c928a5ab7998e1e89a71
Alttaab <alttaab@gmail.com>
2023-10-23 09:17:50 -0700

    Curried adding.

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,10 @@
-function curriedAdd(total) {
-
+function curriedAdd(total = 0) {
+  if (total === 0 && arguments.length === 0) return 0;
+  return function addNext(num) {
+    if (num === undefined) return total;
+    total += num;
+    return addNext;
+  };
 }
 
 module.exports = { curriedAdd };
